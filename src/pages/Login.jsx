0b0c1f1a158754d0ca873c6735bc41bdf15d90c5
MD DANISH RAZA <danishrazamd1@gmail.com>
2026-09-19 23:19:08 +0530

    Redirect after login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   loginWithEmail,
   loginWithGoogle,
@@ -6,6 +7,7 @@ import {
 } from "../firebase/auth";
 
 function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -17,6 +19,7 @@ function Login() {
     try {
       await loginWithEmail(email, password);
       console.log("Login successful");
+      navigate("/profile");
     } catch (error) {
       setError(error.message);
     }
